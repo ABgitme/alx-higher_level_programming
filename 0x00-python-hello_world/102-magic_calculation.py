53f326132d034ccbc1a69e0ddbda79e54bb13e6b
ABgitme <abebekezhabesha@gmail.com>
2023-11-06 15:21:38 +0300

    update tasks 102

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,11 @@
+def magic_calculation(a, b):
+    """_summary_
+
+    Args:
+        a (int): value1
+        b (int): value2
+
+    Returns:
+        int: a + a raised to b
+    """
+    return (98 + a ** b)
