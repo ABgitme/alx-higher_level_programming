cb96d84cbcdc0e681694e90c94ed99e9ba9d6a86
ABgitme <abebekezhabesha@gmail.com>
2023-11-08 00:35:17 +0300

    update 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     if sentence == '':
         sentence = 'None'
-        return len('None'), sentence
+        return 0, None
     else:
         return len(sentence), sentence[0]
