cfe1a811cefe8aab9b5121002aeed40f179ca237
ABgitme <abebekezhabesha@gmail.com>
2023-11-08 00:22:06 +0300

    update 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,6 +2,8 @@
 def max_integer(my_list=[]):
     max_value = my_list[0]
     for element in my_list:
-        if element > max_value:
+        if my_list == '':
+            return 'None'
+        elif element > max_value:
             max_value = element
     return max_value
