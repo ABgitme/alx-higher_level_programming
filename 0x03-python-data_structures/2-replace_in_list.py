01eb6d3420a11fb3f70a1d5cd79bacb53cfad9e9
ABgitme <abebekezhabesha@gmail.com>
2023-11-07 22:26:22 +0300

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list, idx, element):
+def replace_in_list(my_list, idx, element):
     for i, val in enumerate(my_list):
         if idx < 0:
             return my_list
