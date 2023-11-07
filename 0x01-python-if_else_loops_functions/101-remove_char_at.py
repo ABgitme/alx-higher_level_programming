6cef36d2007e98e980f24c7b1e385579bb1172d6
ABgitme <abebekezhabesha@gmail.com>
2023-11-07 09:24:10 +0300

    update

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,2 +1,15 @@
 def remove_char_at(str, n):
-    
+    strlen = len(str)
+    i = 0
+    while i < strlen:
+        if i == n:
+            i += 1
+            continue
+        elif n > strlen or n < 0:
+            print(str, end='')
+            break
+        print(str[i], end='')
+        i += 1
+    print('')
+
+remove_char_at('Best school', 3)
