f9483fbbb6040f9223426080a5469b376852c932
ABgitme <abebekezhabesha@gmail.com>
2023-11-12 10:03:26 +0300

    update task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <lists.h>
+#include "lists.h"
 
 /**
  * check_cycle - checks if singly link has cycle or not
@@ -10,19 +10,24 @@
 */
 int check_cycle(listint_t *list)
 {
-    listint_t *slow = list, *fast = list;
+	listint_t *slow_ptr = list;
+	listint_t *fast_ptr = list;
 
-    if (fast && fast->next)
-        return (0);
-    slow = list->next;
-    fast = list->next->next;
-
-    while (slow && fast && fast->next)
-    {
-        if (slow == fast)
-            return (1);
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-    return (0);
+	/*If the list is empty, then there is no cycle.*/
+	if (list == NULL)
+		return (0);
+	/*Move the two pointers through the list.*/
+	while (fast_ptr != NULL && fast_ptr->next != NULL)
+	{
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next->next;
+		/*If the two pointers ever meet, then there is a cycle.*/
+		if (slow_ptr == fast_ptr)
+			return (1);
+	}
+	/*If the fast pointer reaches the end of the list, then there is no cycle.*/
+	if (fast_ptr == NULL)
+		return (0);
+	/*Otherwise, there must be a cycle in the list.*/
+	return (1);
 }
