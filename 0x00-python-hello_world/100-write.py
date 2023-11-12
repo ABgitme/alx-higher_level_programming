30b36b2b16f7ea6233deae37cd19ed8556509ac2
ABgitme <abebekezhabesha@gmail.com>
2023-11-12 09:45:33 +0300

    update task 10

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
-STRG = 'and that piece of art is useful - Dora Korpar,2015-10-19\n'
+STRG = 'and that piece of art is useful - Dora Korpar, 2015-10-19\n'
 sys.stderr.write(STRG)
 sys.exit(1)
