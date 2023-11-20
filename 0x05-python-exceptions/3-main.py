0391f38b7a9fbf89bfd2eb8969898e24d910946e
ABgitme <abebekezhabesha@gmail.com>
2023-11-20 22:43:56 +0200

    3-safe_print_division

--- 0x05-python-exceptions/3-main.py
+++ 0x05-python-exceptions/3-main.py
@@ -2,7 +2,7 @@
 safe_print_division = __import__('3-safe_print_division').safe_print_division
 
 a = 12
-b = 2
+b = 6
 result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result))
 
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,9 +3,9 @@ def safe_print_division(a, b):
     try:
         c = a / b
         return c
-    except ZeroDivisionError:
+    except ZeroDivisionError or NameError:
         return None
     finally:
-        if ZeroDivisionError:
+        if ZeroDivisionError or NameError:
             c = None
         print("Inside result: {}".format(c))
