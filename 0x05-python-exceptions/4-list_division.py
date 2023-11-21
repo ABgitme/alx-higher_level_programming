6e2dcbd6f3ab0a661ff7b046cb0c79143ee9d9c8
ABgitme <abebekezhabesha@gmail.com>
2023-11-21 19:58:16 +0200

    4-list_division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,5 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
             result_list.append(0)
         finally:
-            if len(result_list)  == list_length:
+            if len(result_list) == list_length:
                 return result_list
