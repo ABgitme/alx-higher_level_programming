6b246d77a554099b680c6d2cadebbfaa7b3a8d61
ABgitme <abebekezhabesha@gmail.com>
2023-11-24 20:03:57 +0200

    1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This class represents a square.
+"""
+
+
+class Square:
+    """
+    Represents a square shape with four equal sides and four right angles.
+
+    Attributes:
+        _size (float): The length of one side of the square.
+    """
+    def __init__(self, size):
+        """
+        Initializes a Square object.
+
+        Args:
+            size (any): The length of one side of the square.
+        """
+        self._size = size
