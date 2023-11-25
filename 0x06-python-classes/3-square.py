2c6956396d2193df5fdfe10ac4db8c145dfa2209
ABgitme <abebekezhabesha@gmail.com>
2023-11-25 18:42:33 +0200

    3-square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+This class represents a square.
+"""
+
+
+class Square:
+    """
+    Represents a square shape with four equal sides and four right angles.
+
+    Attributes:
+        _size (float): The length of one side of the square.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a Square object.
+
+        Args:
+            size (any): The length of one side of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
