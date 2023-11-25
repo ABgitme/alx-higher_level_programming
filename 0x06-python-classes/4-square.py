ddc5f9943947b147bc51a72da1840b8258f5cc5d
ABgitme <abebekezhabesha@gmail.com>
2023-11-25 19:49:58 +0200

    4-square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """
 This class represents a square.
 """
@@ -19,12 +18,42 @@ class Square:
         Args:
             size (any): The length of one side of the square.
         """
+        self._validate_size(size)
+        self.__size = size
+
+    def _validate_size(self, size):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
 
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Retrieves the current size of the square.
+
+        Returns:
+            int: The current size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the new size of the square,
+            validating for an integer and non-negative value.
+
+        Args:
+            new_size (int): The new size to be set.
+
+        Raises:
+            TypeError: If the new size is not an integer.
+            ValueError: If the new size is less than 0.
+        """
+        self._validate_size(value)
+        self.__size = value
+
     def area(self):
         """
         Calculates the area of the square.
@@ -33,6 +62,3 @@ class Square:
             int: The area of the square.
         """
         return self.__size * self.__size
-    @property
-    def size(self):
-        return self.__size
