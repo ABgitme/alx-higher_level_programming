6b4bcc50f6930304a1fd0cdcf1c7ce222e5676d4
ABgitme <abebekezhabesha@gmail.com>
2023-12-01 20:23:46 +0200

    update 6-max_integer_test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -24,7 +24,17 @@ class TestMaxInteger(unittest.TestCase):
         Args:
             None
         """
-        maxval = max_integer(([1, 10, 3, 4]))
+        maxval = max_integer(([1, 10, 3, 15]))
+        self.assertEqual(maxval, 15)
+        maxval = max_integer(([1, 10, 30, 15]))
+        self.assertEqual(maxval, 30)
+        maxval = max_integer(([41, 10, 3, 15]))
+        self.assertEqual(maxval, 41)
+        maxval = max_integer(([40, -40, 3, 15]))
+        self.assertEqual(maxval, 40)
+        maxval = max_integer(([-41,-10, -3, -15]))
+        self.assertEqual(maxval, -3)
+        maxval = max_integer(([10]))
         self.assertEqual(maxval, 10)
         maxval = max_integer([])
         self.assertIsNone(maxval)
