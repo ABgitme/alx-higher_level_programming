e3cc01d819cdbafec658b38af9a8faebcfafc4af
ABgitme <abebekezhabesha@gmail.com>
2024-01-26 14:11:39 +0300

    5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module: base_geometry
+This module defines the BaseGeometry class,
+which serves as a foundation for geometric
+operations and calculations.
+"""
+
+class BaseGeometry:
+    """
+    BaseGeometry class provides a foundation for geometric operations and calculations.
+
+    Attributes:
+    None
+
+    Methods:
+    None
+    """
+    pass
