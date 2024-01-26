688c3c26727b9ccb1004e45a2ba9976c4983e658
ABgitme <abebekezhabesha@gmail.com>
2024-01-26 17:50:58 +0300

    8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module Rectangle class """
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class inherits from BaseGeometry and represents a rectangle shape.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initialize the Rectangle with width and height.
+
+        Args:
+        - width: The width of the rectangle.
+        - height: The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
