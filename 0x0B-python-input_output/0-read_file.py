9ef0177d94df634d6a2d368393a03fa19c1cc902
ABgitme <abebekezhabesha@gmail.com>
2024-01-29 09:42:57 +0300

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module for reading the contents of files.
+
+This module provides the read_file() function, which allows you to read
+the entire content of a specified file and print it to the console.
+"""
+import os
+
+
+def read_file(filename=""):
+    """
+    Reads the content of a file and prints it to the console.
+
+    Args:
+        filename (str, optional): The name or path of the file to read.
+            Defaults to an empty string, indicating the current script.
+
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
