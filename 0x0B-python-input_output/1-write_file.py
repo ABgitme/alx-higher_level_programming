762e65165d203343206f97481220aed807ab229e
ABgitme <abebekezhabesha@gmail.com>
2024-01-29 10:18:39 +0300

    1-write_file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,13 +2,18 @@
 """
 Module for writing text to files in UTF-8 encoding.
 """
+
+
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file (UTF-8) and returns the number of characters written.
+    Writes a string to a text file (UTF-8) and
+    returns the number of characters written.
 
     Args:
-        filename (str, optional): The name of the file to write to. Defaults to an empty string.
-        text (str, optional): The text to write to the file. Defaults to an empty string.
+        filename (str, optional): The name of the file
+            to write to. Defaults to an empty string.
+        text (str, optional): The text to write to the file.
+        Defaults to an empty string.
 
     Returns:
         int: The number of characters written to the file.
