f746e7ef5f50ab6c74da7b98fcf2c55d4e740195
ABgitme <abebekezhabesha@gmail.com>
2024-02-13 18:52:02 +0300

    task 0

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,5 +1,6 @@
+-- Set the default database context to the information_schema database
 USE information_schema;
-
+-- Select the schema_name column from the SCHEMATA table
+-- The SCHEMATA table contains metadata about all databases on the MySQL server
 SELECT schema_name AS "Database"
 FROM SCHEMATA;
-
