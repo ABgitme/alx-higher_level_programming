ffd3c459d4e71b809fd1180554c8f559f53bd85b
ABgitme <abebekezhabesha@gmail.com>
2024-02-13 19:20:28 +0300

    task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Select score and name from the second_table table
+SELECT score, name
+FROM hbtn_0c_0.second_table
+-- Order the records by score in descending order (top first)
+ORDER BY score DESC;
