db23d9785bf7eed780070eb4b330813da018eff7
ABgitme <abebekezhabesha@gmail.com>
2024-02-14 08:37:28 +0300

    task 3 upd

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,4 +1,4 @@
 -- Set the default database context to the specified database name
-SELECT TABLE_NAME
+SELECT TABLE_NAME AS 'Tables_in_hbtn_test_db_0'
 FROM information_schema.tables
 WHERE TABLE_SCHEMA = DATABASE();
