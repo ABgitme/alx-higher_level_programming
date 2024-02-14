14be04011708f81e0b3b80159e859d42e54576a4
ABgitme <abebekezhabesha@gmail.com>
2024-02-14 08:57:24 +0300

    task 5 upd

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,8 +1,13 @@
--- This line selects specific columns from the information_schema.columns table.
-SELECT COLUMN_NAME, DATA_TYPE, COLUMN_KEY, IS_NULLABLE, COLUMN_DEFAULT, EXTRA
--- From the information_schema.columns table which contains various data dictionary information.
+-- Select detailed column information about the specified table:
+SELECT
+    COLUMN_NAME,
+    DATA_TYPE,
+    COLUMN_KEY,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    EXTRA
+-- From the `information_schema.columns` table:
 FROM information_schema.columns
--- Filter results to only include columns belonging to the database passed as an argument.
+-- Filter results based on the current database and the given table name:
 WHERE TABLE_SCHEMA = DATABASE()
--- Further filter to only include columns from the specified table named 'first_table'.
 AND TABLE_NAME = 'first_table';
