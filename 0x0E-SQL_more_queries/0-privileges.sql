452b7b763c932afc92b3f264b51b487d8d3e7183
ABgitme <abebekezhabesha@gmail.com>
2024-02-14 23:34:00 +0300

    task 0 update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,15 +1,3 @@
--- Check if necessary information_schema table exists in your MySQL version
-SELECT COUNT(*) AS table_exists
-FROM information_schema.user_grants;
-
-IF table_exists > 0 THEN
-    -- List privileges for user_0d_1
-    SELECT *
-    FROM information_schema.user_grants
-    WHERE user = 'user_0d_1' AND host = '%';
-    -- List privileges for user_0d_2
-    SELECT *
-    FROM information_schema.user_grants
-    WHERE user = 'user_0d_2' AND host = '%';
-END IF;
-
+-- Show grant of information_schema table in your MySQL version
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
