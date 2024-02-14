9856aeb4184f93a88c89516cbcd42ec8854d2d7b
ABgitme <abebekezhabesha@gmail.com>
2024-02-15 00:51:24 +0300

    task 15 and 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,3 +1,4 @@
+-- Retrieve the titles of TV shows and their corresponding genres
 SELECT t.title, g.name  -- Select the title from tv_shows and the name from tv_genres
 FROM tv_shows t  -- Alias tv_shows table as t
 LEFT JOIN tv_show_genres s ON t.id = s.show_id  -- Left join tv_show_genres table as s based on show_id
