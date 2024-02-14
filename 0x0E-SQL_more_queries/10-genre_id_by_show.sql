81f3ab4f04541ed575ae21d573e11a4246207af7
ABgitme <abebekezhabesha@gmail.com>
2024-02-15 00:20:04 +0300

    task 10 update

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,9 @@
--- Store database name as a variable
-SELECT 
-  tv_shows.title,
-  tv_show_genres.genre_id
-FROM $database.tv_shows
-INNER JOIN $database.tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-GROUP BY tv_shows.title, tv_show_genres.genre_id
+-- Select tv_shows.title and tv_show_genres.genre_id from tv_shows and tv_show_genres tables
+-- Join tv_shows and tv_show_genres tables using tv_shows.id = tv_show_genres.tv_show_id
+-- Filter records to include only shows that have at least one genre linked
+-- Display tv_shows.title and tv_show_genres.genre_id in each record
+-- Sort the results in ascending order by tv_shows.title and tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
