b27a2cbbff71f4e5a813d2e73163378f8a894a9d
ABgitme <abebekezhabesha@gmail.com>
2024-02-15 00:30:36 +0300

    task 11 update

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,10 @@
--- Select tv_shows.title and tv_show_genres.genre_id from tv_shows and tv_show_genres tables
--- LEFT JOIN tv_show_genres on tv_shows.id = tv_show_genres.tv_show_id to include shows with no genre
--- Display tv_shows.title and tv_show_genres.genre_id in each record, showing NULL if no genre exists
--- Sort the results in ascending order by tv_shows.title and tv_show_genres.genre_id
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- Select the title from tv_shows and the genre_id from tv_show_genres
+SELECT s.title, g.genre_id
+-- Alias tv_shows table as s
+FROM tv_shows s
+-- Left join tv_show_genres table as g
+LEFT JOIN tv_show_genres g
+-- Match records where the id from tv_shows matches the show_id from tv_show_genres
+ON s.id = g.show_id
+-- Order the results by title and genre_id in ascending order
+ORDER BY s.title ASC, g.genre_id ASC;
