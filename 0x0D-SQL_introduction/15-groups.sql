90e3451e2d15de912c314a452cb117d2a7bf171b
ABgitme <abebekezhabesha@gmail.com>
2024-02-15 17:59:16 +0300

    task 15 upd

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,8 @@
+-- List the number of records with the same score in the second_table
+SELECT score, COUNT(*) AS number_of_records
+-- Specify the table and database
+FROM second_table
+-- Group records by score
+GROUP BY score
+-- Order the results by the number of records in descending order
+ORDER BY number_of_records DESC;
