8dbf564d4c7965838e4b8e1d21479f000928c7ea
ABgitme <abebekezhabesha@gmail.com>
2024-02-15 14:40:12 +0300

    task 9 upd

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,11 @@
 -- Create the table if it does not exist
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+CREATE TABLE IF NOT EXISTS DATABASE().second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
 -- Insert multiple rows into the second_table table
-INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
+INSERT INTO DATABASE().second_table (id, name, score) VALUES
 (1, 'John', 10),
 (2, 'Alex', 3),
 (3, 'Bob', 14),
