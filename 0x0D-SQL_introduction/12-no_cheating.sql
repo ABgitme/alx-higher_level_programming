5071631b6b14d61b60692269998246d8ba3950b0
ABgitme <abebekezhabesha@gmail.com>
2024-02-15 15:42:06 +0300

    task 12 upd

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,6 @@
+-- Update the score of Bob to 10 in the table second_table
+UPDATE second_table
+-- Set the score value to 10
+SET score = 10
+-- Where the name field matches 'Bob'
+WHERE name = 'Bob';
