f13477e5fbb5c05939ff6779b1faffec89ab6cc5
ABgitme <abebekezhabesha@gmail.com>
2024-02-15 14:30:17 +0300

    task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,13 +1,2 @@
--- Select detailed column information about the specified table:
-SELECT
-    COLUMN_NAME,
-    DATA_TYPE,
-    COLUMN_KEY,
-    IS_NULLABLE,
-    COLUMN_DEFAULT,
-    EXTRA
--- From the `information_schema.columns` table:
-FROM information_schema.columns
--- Filter results based on the current database and the given table name:
-WHERE TABLE_SCHEMA = DATABASE()
-AND TABLE_NAME = 'first_table';
+-- show detailed full description of specified table:
+SHOW CREATE TABLE first_table;
