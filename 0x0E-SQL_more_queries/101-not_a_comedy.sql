7a73e89901f536712978ec5b11912a16a590a065
ABgitme <abebekezhabesha@gmail.com>
2024-02-15 20:26:59 +0300

    task 101 upd

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,7 +2,7 @@
 SELECT DISTINCT t.title
 FROM tv_shows AS t
 -- Left join with tv_show_genres to include all shows, even those without genres
-LEFT JOIN tv_show_genres AS s ON s.show_id = t.`id`
+LEFT JOIN tv_show_genres AS s ON s.show_id = t.id
 -- Left join with tv_genres to check for shows in the Comedy genre
 LEFT JOIN tv_genres AS g ON g.id = s.genre_id
 -- Filter out shows that are in the Comedy genre
