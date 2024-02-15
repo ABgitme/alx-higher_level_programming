b7c7ffe78ac3ed88ac89ca780e68db1bd8b2de71
ABgitme <abebekezhabesha@gmail.com>
2024-02-15 20:37:00 +0300

    task 101 upd

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,10 +1,10 @@
--- Retrieve distinct titles of shows that are not in the Comedy genre
-SELECT DISTINCT t.title
-FROM tv_shows AS t
--- Left join with tv_show_genres to include all shows, even those without genres
-LEFT JOIN tv_show_genres AS s ON s.show_id = t.id
--- Left join with tv_genres to check for shows in the Comedy genre
-LEFT JOIN tv_genres AS g ON g.id = s.genre_id
--- Filter out shows that are in the Comedy genre
-WHERE g.name IS NULL OR g.name != "Comedy"
-ORDER BY t.title;
+-- Replace `<database>` with the actual database name
+SET @database = '<hbtn_0d_tvshows>';
+
+-- Select shows not linked to the "Comedy" genre (id: 5)
+SELECT s.title
+FROM @database.tv_shows AS s
+LEFT JOIN @database.tv_show_genres AS g ON s.id = g.show_id
+WHERE g.genre_id IS NULL OR g.genre_id != 5;
+-- Order results by title in ascending order
+ORDER BY s.title ASC;
