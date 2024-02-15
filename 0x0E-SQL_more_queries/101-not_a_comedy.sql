e23113d5c01bb7a9df2dc3daabf8ebd4f98cc101
ABgitme <abebekezhabesha@gmail.com>
2024-02-15 20:17:37 +0300

    task 101 upd

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,17 +2,9 @@
 SELECT title
 -- From the tv_shows table
 FROM tv_shows
--- Filter the shows based on their IDs that are not associated with the Comedy genre
-WHERE id NOT IN (
-    -- Subquery to retrieve the IDs of shows associated with the Comedy genre
-    SELECT show_id
-    FROM tv_show_genres
-    WHERE genre_id = (
-        -- Subquery to retrieve the ID of the Comedy genre
-        SELECT id
-        FROM tv_genres
-        WHERE name = 'Comedy'
-    )
-)
+-- Left join with the tv_show_genres table to check if the show has the Comedy genre
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    -- Filter out the shows that have the Comedy genre
+WHERE tv_show_genres.genre_id IS NULL
 -- Order the results in ascending order by the show title
 ORDER BY title;
