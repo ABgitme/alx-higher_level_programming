5fdd2bfa73ca5512adb3cf4f4a9c4e6ae89872b8
ABgitme <abebekezhabesha@gmail.com>
2024-02-15 20:38:04 +0300

    task 101 upd

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,10 +1,7 @@
--- Replace `<database>` with the actual database name
-SET @database = '<hbtn_0d_tvshows>';
-
 -- Select shows not linked to the "Comedy" genre (id: 5)
 SELECT s.title
-FROM @database.tv_shows AS s
-LEFT JOIN @database.tv_show_genres AS g ON s.id = g.show_id
+FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS g ON s.id = g.show_id
 WHERE g.genre_id IS NULL OR g.genre_id != 5;
 -- Order results by title in ascending order
 ORDER BY s.title ASC;
