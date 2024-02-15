bb9c9ca948eada16569f4d8f402f50734d7df5b7
ABgitme <abebekezhabesha@gmail.com>
2024-02-15 18:28:08 +0300

    task 101 upd

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,8 +1,8 @@
 -- Select the city and calculate the average temperature (Fahrenheit) for each city
-SELECT city, AVG(temperature) AS avg_temperature_fahrenheit
+SELECT city, AVG(temperature) AS avg_temp
 -- Specify the table and database
 FROM temperatures
 -- Group the data by city
 GROUP BY city
 -- Order the results by the average temperature in descending order
-ORDER BY avg_temperature_fahrenheit DESC;
+ORDER BY avg_temp DESC;
