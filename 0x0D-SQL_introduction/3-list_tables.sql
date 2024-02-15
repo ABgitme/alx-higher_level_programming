07e7005533cf211efb9a3d0a0388d631767a3293
ABgitme <abebekezhabesha@gmail.com>
2024-02-15 14:17:29 +0300

    task 3

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,4 +1,4 @@
--- Store database name in a variable
-SET @database = $database;
--- Show tables in the specified database
-SHOW TABLES FROM @database;
+-- Set the default database context to the specified database name
+SELECT TABLE_NAME AS 'Tables_in_hbtn_test_db_0'
+FROM information_schema.tables
+WHERE TABLE_SCHEMA = DATABASE();
