c11ce76b54bdc7e0004a096cfe130b11cbbd76a3
ABgitme <abebekezhabesha@gmail.com>
2024-03-11 19:48:11 +0300

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-// Iterate through each command-line argument
 // Access arguments using process.argv (without 'var')
 const firstArg = process.argv[2];
 const secondArg = process.argv[3];
-
 // Print arguments with a space (" ") before "is"
 console.log(`${firstArg} is ${secondArg}`);
