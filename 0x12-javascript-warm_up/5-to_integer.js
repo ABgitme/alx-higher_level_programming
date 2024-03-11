fd9398a7564e6bcac71718843c7c7dc92a689eba
ABgitme <abebekezhabesha@gmail.com>
2024-03-11 20:14:12 +0300

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 if (!isNaN(Number(process.argv[2]))) {
   const firstArg = process.argv[2];
-  console.log('My number: ' + firstArg);
+  console.log('My number: ' + parseInt(firstArg));
 } else {
   console.log('No argument');
 }
