3f53c703d973bf7c3bad94e88d89180d96e175b1
ABgitme <abebekezhabesha@gmail.com>
2024-03-11 17:56:58 +0300

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
 
 // Check if no arguments are passed
-if (process.argv.length <= 2) {
+let counter = 0;
+
+// Iterate through each command-line argument
+process.argv.forEach((arg, index) => {
+  if (index === 2) {
+    console.log(arg);
+  }
+  counter = counter + 1;
+});
+
+// If no arguments are passed, print "No argument"
+if (counter <= 2) {
   console.log('No argument');
 }
-  // Iterate through each command-line argument
-  process.argv.forEach((arg, index) => {
-    if (index === 2) {
-      console.log(arg);
-    }
-  });
