17f4d988f0c31b67414d0e3b42e656d6ed7763fd
ABgitme <abebekezhabesha@gmail.com>
2024-03-11 20:22:49 +0300

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const myStringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < myStringArray.length; i++) {
+  console.log(myStringArray[i]);
+}
