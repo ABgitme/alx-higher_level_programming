7c9791a5bb209c260504a04986bbcc284dea371e
ABgitme <abebekezhabesha@gmail.com>
2024-03-14 22:07:40 +0300

    10-model_state_my_get

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    # Check if correct number of arguments provided
+
+    # Get MySQL credentials and database name from command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Create engine
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost/{}".format(
+                username, password, database, pool_pre_ping=True
+                )
+            )
+    Base.metadata.create_all(engine)
+
+    # Create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    filtered = session.query(State).filter(State.name == state_name).first()
+    # Print the State objects
+    if filtered:
+        print("{}".format(filtered.id))
+    else:
+        print("Nothing")
+
+    session.close()
