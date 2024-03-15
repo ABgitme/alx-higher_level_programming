1869993a1bcf89be669f4dca5fa083c5d80333c3
ABgitme <abebekezhabesha@gmail.com>
2024-03-15 07:52:58 +0300

    8-model_state_fetch_first upd doc

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""
+This script connects to a MySQL database, queries the `State` table,
+and prints the first state record if it exists.
+
+It takes the following arguments:
+  1. username: Username for the MySQL database.
+  2. password: Password for the MySQL database.
+  3. database: Name of the MySQL database to connect to.
+
+The script utilizes SQLAlchemy to establish a connection to the database,
+create a session, and perform the query. The first state object is retrieved
+and printed if found. Otherwise, a message indicating "Nothing" is printed.
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
