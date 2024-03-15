11ce0cf8740c8d34c93291d01e26b3789a6135a2
ABgitme <abebekezhabesha@gmail.com>
2024-03-15 08:11:50 +0300

    12-model_state_update_id_2 upd doc

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+"""
+This script updates the name of a specific state in a MySQL database
+using SQLAlchemy.
+
+It retrieves the state with the provided ID (2 in this case) and
+updates its name.
+The script expects the following command-line arguments:
+
+1. Username: The username for accessing the MySQL database.
+2. Password: The password for accessing the MySQL database.
+3. Database name: The name of the database containing the states table.
+
+The script utilizes the `model_state` module which defines the database model
+for states.
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
