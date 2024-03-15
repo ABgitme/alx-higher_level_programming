6cb6c3a7c59a0699b060e14c547baf26a2489766
ABgitme <abebekezhabesha@gmail.com>
2024-03-15 08:32:22 +0300

    relationship_state upd doc

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,10 +1,36 @@
 #!/usr/bin/python3
+"""
+This module defines a SQLAlchemy model for
+representing cities in a database.
+
+The module utilizes the SQLAlchemy declarative base
+class (`Base`) from the `relationship_state` module
+to create a model named `City`.
+
+The `City` class represents a row in the `cities`
+table of a database and has a foreign key relationship
+with the `State` model.
+"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from relationship_state import Base
 
 
 class City(Base):
+    """
+    This class represents a city entity in a database.
+
+    It maps to the `cities` table and has the following attributes:
+
+    - id: (Integer, Primary Key, Not Null) The unique identifier for the city.
+    - name: (String, Not Null) The name of the
+        city (maximum length of 128 characters).
+    - state_id: (Integer, Foreign Key)
+        The foreign key referencing the id of the
+        associated state (references the states.id column).
+    - state: (Relationship) A relationship with the `State` model, allowing
+        access to the related state object.
+    """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+This module defines a SQLAlchemy model for representing
+states and their associated cities in a database.
+
+The module utilizes the SQLAlchemy declarative
+base class to create a model named `State`.
+The `State` class represents a row in the `states` table of a database,
+and it can have a relationship with multiple `City` objects.
+
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,6 +17,24 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    This class represents a state entity in a database.
+
+    It maps to the `states` table and has the following attributes:
+
+    - id: (Integer, Primary Key, Not Null) The unique identifier for the state.
+    - name: (String, Not Null) The name of the state
+        (maximum length of 128 characters).
+    - cities: (relationship) A relationship with the `City` class.
+        It specifies a one-to-many relationship
+        between a `State` and its associated `City` objects.
+        The `back_populates` argument defines
+        the backreference on the `City` model (usually a field named `state`).
+        The `cascade` argument specifies that operations
+        on a `State` object should also cascade
+        to its related `City` objects
+        (e.g., deleting a state also deletes its cities).
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
