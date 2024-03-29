ba1ad02c33fedfb8adaa73a23f2b2a7ccc712b30
ABgitme <abebekezhabesha@gmail.com>
2024-03-29 04:26:57 +0300

    0-body_size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-response=$(curl -sI "$1")
-
 # Extract Content-Length from the response headers
-content_length=$(echo "$response" | grep -iE '^Content-Length:' | awk '{print $2}')
-echo "$content_length"
+curl -sI "$1" | grep -iE '^Content-Length:' | awk '{print $2}'
