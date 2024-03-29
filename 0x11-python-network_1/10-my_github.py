825986222dde37a9130cdc3d1680c98e8a2f0e75
ABgitme <abebekezhabesha@gmail.com>
2024-03-29 11:37:23 +0300

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module: fetch_github_id
+
+Description:
+This script takes GitHub credentials (username and personal access token)
+    and uses the GitHub API to display the user's id.
+It uses Basic Authentication with the provided username and
+    personal access token to access the user's information.
+
+Usage:
+./fetch_github_id.py <username> <personal_access_token>
+
+Arguments:
+- <username>: Your GitHub username.
+- <personal_access_token>: Your GitHub personal access token.
+
+Requirements:
+- Uses the packages requests and sys.
+- Does not import packages other than requests and sys.
+"""
+import requests
+import sys
+
+
+username = sys.argv[1]
+password = sys.argv[2]
+
+url = 'https://api.github.com/user'
+
+response = requests.get(url, auth=(username, password))
+
+if response.status_code == 200:
+    user_data = response.json()
+    print("{}".format(user_data['id']))
+else:
+    print("None")
