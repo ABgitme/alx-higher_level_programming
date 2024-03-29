54fc9125fc39133b37bfbfac26e2ede6bb8683e6
ABgitme <abebekezhabesha@gmail.com>
2024-03-29 05:46:27 +0300

    102-catch_me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send request to 0.0.0.0:5000/catch_me and capture the response
-curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
+curl -s -X PUT -o /dev/null -w "You got me!" 0.0.0.0:5000/catch_me
