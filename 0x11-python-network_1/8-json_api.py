aad8fc122e9c1016706dd0216d1e29dd9bb8397a
ABgitme <abebekezhabesha@gmail.com>
2024-03-29 11:28:08 +0300

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Module: search_user
+
+Description:
+This script takes in a letter and sends a POST request
+    to http://0.0.0.0:5000/search_user with the letter as a parameter.
+If the letter is not provided, it sets the parameter q="".
+If the response body is properly JSON formatted and not empty,
+    it displays the id and name.
+Otherwise, it displays appropriate error messages.
+
+Usage:
+./search_user.py <letter>
+
+Arguments:
+- <letter>: The letter to be sent as a parameter in the POST request.
+    If not provided, defaults to an empty string.
+
+Requirements:
+- Uses the packages requests and sys.
+- Does not import packages other than requests and sys.
+"""
+import requests
+import sys
+
+
+if len(sys.argv) > 1:
+    q = sys.argv[1]
+else:
+    q = ""
+
+url = 'http://0.0.0.0:5000/search_user'
+data = {'q': q}
+
+response = requests.post(url, data=data)
+
+try:
+    json_response = response.json()
+    if json_response:
+        print("[{}] {}".format(json_response['id'], json_response['name']))
+    else:
+        print("No result")
+except ValueError:
+    print("Not a valid JSON")
