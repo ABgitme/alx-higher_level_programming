99a8f961a5671bfdbd488ca455861e99cedbd7b8
ABgitme <abebekezhabesha@gmail.com>
2024-03-30 04:02:07 +0300

    100-github_commits  upd

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -27,7 +27,8 @@ if __name__ == "__main__":
 
     repository_name = sys.argv[1]
     owner_name = sys.argv[2]
-    url = f"https://api.github.com/repos/{owner_name}/{repository_name}/commits"
+    url = \
+        f"https://api.github.com/repos/{owner_name}/{repository_name}/commits"
 
     response = requests.get(url)
 
