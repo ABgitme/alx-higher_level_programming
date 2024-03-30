4a9a1205ea61b67f5ac3ba02b7af6d513fee49d2
ABgitme <abebekezhabesha@gmail.com>
2024-03-30 03:20:37 +0300

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -39,6 +39,6 @@ url = sys.argv[1]
 
 if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
-        x_request_id = response.headers.get('X-Request-Id')
+        x_request_id = response.info().get('X-Request-Id')
         if x_request_id:
             print(x_request_id)
