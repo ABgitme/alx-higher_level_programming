a05636c84bc0c006db2e829b76b4bec343d018d0
ABgitme <abebekezhabesha@gmail.com>
2024-03-30 03:48:15 +0300

    8-json_api  upd

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -31,15 +31,11 @@ else:
     q = ""
 
 url = 'http://0.0.0.0:5000/search_user'
-data = {'q': q}
+response = requests.post(url, data={'q': q})
 
-response = requests.post(url, data=data)
-
-try:
+if __name__ == "__main__":
     json_response = response.json()
     if json_response:
         print("[{}] {}".format(json_response['id'], json_response['name']))
     else:
         print("No result")
-except ValueError:
-    print("Not a valid JSON")
