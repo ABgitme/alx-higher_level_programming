8796a45adde70b1861ae54c7c78cb4a010bd92a5
ABgitme <abebekezhabesha@gmail.com>
2024-03-30 03:51:17 +0300

    8-json_api  upd

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -25,15 +25,14 @@ import requests
 import sys
 
 
-if len(sys.argv) > 1:
-    q = sys.argv[1]
-else:
-    q = ""
-
-url = 'http://0.0.0.0:5000/search_user'
-response = requests.post(url, data={'q': q})
-
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+    url = 'http://0.0.0.0:5000/search_user'
+    response = requests.post(url, data={'q': q})
     json_response = response.json()
     if json_response:
         print("[{}] {}".format(json_response['id'], json_response['name']))
