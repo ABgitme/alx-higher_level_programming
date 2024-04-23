ae255cedb0acc3b4019aef65dab3cefa45c29cc3
ABgitme <abebekezhabesha@gmail.com>
2024-04-23 23:38:46 +0200

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,19 +4,20 @@ const request = require('request');
 // Get the API URL from the command line arguments
 const apiUrl = process.argv[2];
 
-// Character ID for Wedge Antilles
-const characterId = '18';
-
 // Make a GET request to the provided Star Wars API URL
-request.get(apiUrl, (error, response, body) => {
+request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
-  } else {
-    const films = JSON.parse(body).results;
-    // Count the number of movies where Wedge Antilles is present
-    const moviesWithWedge = films.filter(film =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    ).length;
-    console.log(`${moviesWithWedge}`);
+    return;
   }
+
+  const movies = JSON.parse(body).results;
+  const count = movies.reduce((acc, movie) => {
+    if (movie.characters.some(character => character.endsWith('/18/'))) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+
+  console.log(count);
 });
