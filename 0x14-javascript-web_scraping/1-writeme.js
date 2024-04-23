fbd9e91f2326a1ab1e456d1bbc5b6e7bc23fa174
ABgitme <abebekezhabesha@gmail.com>
2024-04-23 22:30:35 +0200

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,11 +5,6 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-// Check if both file path and content are provided as arguments
-if (!filePath || !content) {
-  process.exit(1);
-}
-
 // Write the content to the file
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
