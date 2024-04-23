1814e5d885327ba7f38e5e9e54678fd49200e829
ABgitme <abebekezhabesha@gmail.com>
2024-04-23 22:40:11 +0200

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+
+// Get the URL from the command line arguments
+const url = process.argv[2];
+
+// Make a GET request to the provided URL
+request.get(url, (error, response) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
