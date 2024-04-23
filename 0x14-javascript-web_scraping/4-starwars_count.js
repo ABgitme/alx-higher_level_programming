5ff7386eb08e4896af96785e9f663a971762cd31
ABgitme <abebekezhabesha@gmail.com>
2024-04-23 23:29:19 +0200

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,12 +4,6 @@ const request = require('request');
 // Get the API URL from the command line arguments
 const apiUrl = process.argv[2];
 
-// Check if an API URL is provided as an argument
-if (!apiUrl) {
-  console.error('Please provide the API URL as an argument.');
-  process.exit(1);
-}
-
 // Character ID for Wedge Antilles
 const characterId = '18';
 
